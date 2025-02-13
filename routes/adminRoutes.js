27375e197b99fea03992ff032abb37d8570d1d41
Akku-2325 <231696@astanait.edu.kz>
2025-02-13 06:01:11 +0500

    Added link to admin panel on main page

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/../views/admin'); // Если HTML
+    res.sendFile(__dirname + '/../views/admin/index'); // Если HTML
     // res.render('adminPanel'); // Если EJS
 });
 
