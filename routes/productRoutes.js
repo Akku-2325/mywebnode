c85ff9b44e32778d2001476946868e6f24032ca4
Akku-2325 <231696@astanait.edu.kz>
2025-02-13 21:02:06 +0500

    Updates

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,15 +3,16 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Public routes
-router.get('/', productController.getAllProducts);
-router.get('/:id', productController.getProductById);
-
 // Admin-protected routes
 router.get('/create', authMiddleware.isLoggedIn, authMiddleware.isAdmin, productController.getCreateProductForm);
 router.post('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, productController.createProduct);
 router.get('/:id/edit', authMiddleware.isLoggedIn, authMiddleware.isAdmin, productController.getEditProductForm);
 router.put('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, productController.updateProduct);
 router.delete('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, productController.deleteProduct);
+
+// Public routes
+router.get('/', productController.getAllProducts);
+router.get('/:id', productController.getProductById);
+
 
 module.exports = router;
