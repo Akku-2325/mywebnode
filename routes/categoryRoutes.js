3bd3e8989a6b36278ac084ee6de9598ccc22647f
Akku-2325 <231696@astanait.edu.kz>
2025-02-13 21:10:31 +0500

    Updates

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,19 +3,15 @@ const router = express.Router();
 const categoryController = require('../controllers/categoryController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Public routes
-router.get('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.getAllCategories);
-router.get('/:id', categoryController.getCategoryById);
-
-// Admin routes
+// Admin routes (create and edit) - MUST be before /:id routes
 router.get('/create', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.getCreateCategoryForm);
 router.post('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.createCategory);
-router.put('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.updateCategory);
-router.delete('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.deleteCategory);
-
-// Admin routes for editing
 router.get('/:id/edit', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.editCategory);
 router.put('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.updateCategory);
 router.delete('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.deleteCategory);
+
+// Public routes
+router.get('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.getAllCategories); // Список категорий
+router.get('/:id', categoryController.getCategoryById); // Конкретная категория
 
 module.exports = router;
