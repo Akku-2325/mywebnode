e8df07e7fd396ee80805ee4fbe5bb90fb0c921eb
Akku-2325 <231696@astanait.edu.kz>
2025-02-13 22:58:05 +0500

    Updates

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,8 +4,8 @@ const categoryController = require('../controllers/categoryController');
 const authMiddleware = require('../middleware/authMiddleware'); // Предполагаю, что у тебя есть middleware для проверки авторизации
 
 // Admin routes (create and edit) - MUST be before /:id routes
-router.get('/create', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.getCreateCategoryForm); // authMiddleware added
-router.post('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.createCategory); // authMiddleware added
+router.get('/create', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.getCreateCategoryForm);
+router.post('/', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.createCategory);
 router.get('/:id/edit', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.editCategory); // authMiddleware added
 router.put('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.updateCategory); // authMiddleware added
 router.delete('/:id', authMiddleware.isLoggedIn, authMiddleware.isAdmin, categoryController.deleteCategory); // authMiddleware added
