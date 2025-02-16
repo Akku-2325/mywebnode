2e7065113606407d0e3ae4de6f828da7839432d1
Akku-2325 <231696@astanait.edu.kz>
2025-02-16 22:45:35 +0500

    style

--- public/main.js
+++ public/main.js
@@ -5,11 +5,11 @@ const Setting = require('../models/Setting');
 router.get('/', async (req, res) => {
     try {
         const bannerSetting = await Setting.findOne({ key: 'bannerImageUrl' });
-        const bannerImageUrl = bannerSetting ? bannerSetting.value : '/images/default-banner.jpg'; // Default URL
+        const bannerImageUrl = bannerSetting ? bannerSetting.value : '/banner/default-banner.jpg'; // Default URL
         res.render('main', { user: req.session.user, bannerImageUrl: bannerImageUrl });
     } catch (error) {
         console.error('Error fetching banner image URL:', error);
-        res.render('main', { user: req.session.user, bannerImageUrl: '/images/default-banner.jpg' }); // Default URL
+        res.render('main', { user: req.session.user, bannerImageUrl: '/banner/default-banner.jpg' }); // Default URL
     }
 });
 
