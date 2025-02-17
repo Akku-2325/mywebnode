e0934aaf51d90fa6c042253879413fe827ddd46b
Akku-2325 <231696@astanait.edu.kz>
2025-02-17 05:50:02 +0500

    cart

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const fs = require('fs'); // Import the fs module
 const authMiddleware = require('./middleware/authMiddleware');
 const methodOverride = require('method-override'); // Import method-override
 const Setting = require('./models/Setting'); // Add Setting model
+const Product = require('./models/Product'); // Import the Product model
 const cloudinary = require('cloudinary').v2; // Cloudinary
 
 const app = express();
@@ -31,12 +32,11 @@ app.use(session({
 app.use((req, res, next) => {
     console.log('Session:', req.session);
     next();
- });
+});
 
 const authRoutes = require('./routes/authRoutes');
 const userProductRoutes = require('./routes/userProductRoutes');
 const cartRoutes = require('./routes/cartRoutes');
-
 
 // Middleware
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,22 +44,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-app.use('/productList', userProductRoutes); 
+app.use('/productList', userProductRoutes);
 app.use('/cart', cartRoutes);
 
 app.use(methodOverride('_method')); // Add method-override middleware
-
-// Session Configuration
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI,
-        ttl: 14 * 24 * 60 * 60,
-        autoRemove: 'native'
-    })
-}));
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -73,39 +61,26 @@ mongoose.connect(process.env.MONGODB_URI, {
     })
     .catch(err => console.error('MongoDB connection error:', err));
 
-// Middleware to check if user is logged in
 const isLoggedIn = (req, res, next) => {
     if (req.session.userId) {
         next();
     } else {
-        res.redirect('/login');
+        res.redirect('/auth/login');
     }
 };
-
-// Apply setUser middleware to all routes
-// app.use(async (req, res, next) => {
-//     try {
-//         await authMiddleware.setUser(req, res, next);
-//     } catch (error) {
-//         console.error('Error in setUser middleware:', error);
-//         // Handle the error gracefully, e.g., redirect to an error page
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
 
 // Routes
 app.use('/auth', authRoutes);
 
-//  Add your new routes here, e.g.:
 const productRoutes = require('./routes/productRoutes');
 app.use('/products', productRoutes);
+
 const categoryRoutes = require('./routes/categoryRoutes');
 app.use('/categories', categoryRoutes);
 
 const adminRoutes = require('./routes/adminRoutes'); //Add admin Routes
 app.use('/admin', adminRoutes);
 
-// Route to handle profile rendering
 app.get('/admin', isLoggedIn, authMiddleware.isAdmin, async (req, res) => {
     try {
         res.render('admin/index');
@@ -115,7 +90,6 @@ app.get('/admin', isLoggedIn, authMiddleware.isAdmin, async (req, res) => {
     }
 });
 
-// Protected route example (Profile management - moved here for now)
 app.get('/profile', isLoggedIn, async (req, res) => {
     try {
         const user = await User.findById(req.session.userId).lean();
@@ -124,13 +98,12 @@ app.get('/profile', isLoggedIn, async (req, res) => {
 
         res.render('profile', {
             user: user,
-            // notes: notes, // Removed
             editing: editMode,
             errors: []
         });
     } catch (error) {
         console.error('Error fetching data:', error);
-        res.render('profile', { user: req.session.user, /* notes: [], */ error: 'Failed to fetch profile.', editing: false, errors: [] }); // Updated
+        res.render('profile', { user: req.session.user, error: 'Failed to fetch profile.', editing: false, errors: [] }); // Updated
     }
 });
 
@@ -148,7 +121,6 @@ app.post('/profile/edit', isLoggedIn, [
             const user = await User.findById(req.session.userId).lean()
             return res.render('profile', {
                 user: user,
-                // notes: notes, // Removed
                 editing: true,
                 errors: errors.array()
             });
@@ -192,19 +164,19 @@ app.post('/profile/edit', isLoggedIn, [
 });
 
 app.post('/profile/delete', isLoggedIn, async (req, res) => {
-  try {
-      await User.findByIdAndDelete(req.session.userId);
-      req.session.destroy((err) => {
-          if (err) {
-              console.error('Error destroying session:', err);
-              return res.redirect('/');
-          }
-          res.redirect('/auth/register');
-      });
-  } catch (error) {
-      console.error('Error deleting account:', error);
-      res.status(500).send('An error occurred while deleting the account.');
-  }
+    try {
+        await User.findByIdAndDelete(req.session.userId);
+        req.session.destroy((err) => {
+            if (err) {
+                console.error('Error destroying session:', err);
+                return res.redirect('/');
+            }
+            res.redirect('/auth/register');
+        });
+    } catch (error) {
+        console.error('Error deleting account:', error);
+        res.status(500).send('An error occurred while deleting the account.');
+    }
 });
 
 app.post('/profile/remove-picture', isLoggedIn, async (req, res) => {
@@ -266,23 +238,24 @@ app.post('/profile/upload', isLoggedIn, async (req, res) => {
 });
 
 // Main route that renders banner and adds user
-app.get('/',authMiddleware.redirectIfAdmin, async (req, res) => {
-  try {
-    const bannerSetting = await Setting.findOne({ key: 'bannerImageUrl' });
-    const bannerImageUrl = bannerSetting ? bannerSetting.value : '/banner/default-banner.jpg'; // URL по умолчанию
+app.get('/', authMiddleware.redirectIfAdmin, async (req, res) => {
+    try {
+        const bannerSetting = await Setting.findOne({ key: 'bannerImageUrl' });
+        const bannerImageUrl = bannerSetting ? bannerSetting.value : '/banner/default-banner.jpg'; // URL по умолчанию
+        const products = await Product.find().populate('category'); // Fetch all products from the database
 
-    if (req.session.userId) {
-      // User is logged in, render the main page
-      res.render('main', { user: res.locals.user, bannerImageUrl: bannerImageUrl });
-    } else {
-      // User is not logged in, render the index page
-      res.render('index', { bannerImageUrl: bannerImageUrl });
+        if (req.session.userId) {
+            // User is logged in, render the main page
+            res.render('main', { user: req.session.user, bannerImageUrl: bannerImageUrl, products: products });
+        } else {
+            // User is not logged in, render the index page
+            res.render('index', { bannerImageUrl: bannerImageUrl, products: products });
+        }
+    } catch (error) {
+        console.error('Error fetching banner image URL or products:', error);
+        // Handle the error, e.g., by rendering with a default banner or an error message
+        res.render('index', { bannerImageUrl: '/banner/default-banner.jpg', products: [] }); // Render index by default
     }
-  } catch (error) {
-    console.error('Error fetching banner image URL:', error);
-    // Handle the error, e.g., by rendering with a default banner or an error message
-    res.render('index', { bannerImageUrl: '/banner/default-banner.jpg' }); // Render index by default
-  }
 });
 
 app.listen(port, () => {
